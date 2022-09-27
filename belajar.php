7e222bc577b9571c94d7e29387c5bf0f9677c759
raupsss <androghanz@gmail.com>
2022-09-27 10:10:50 +0700

    belajar

--- belajar.php
+++ belajar.php
@@ -1,7 +1,12 @@
 <?php
-$numbers = [3, 4, 1, 65, 32, 6, 21];
+$numbers = [3, 4, 1, 65, 32, 6, 21, 100, 200, 300];
 $numbers[] = 22;
 ?>
+<?php $students = [
+    ["Raup", "12345", "TKJ", "raup@gmail.com"], 
+    ["Ghani", "12345", "TKJ", "ghani@gmail.com"]
+    
+    ]; ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -30,6 +35,7 @@ $numbers[] = 22;
 </head>
 
 <body>
+
     <!-- Perulangan FOR -->
     <h4>Perulangan For</h4>
     <?php for ($i = 0; $i < count($numbers); $i++) { ?>
@@ -50,6 +56,24 @@ $numbers[] = 22;
     <?php foreach ($numbers as $number) : ?>
         <div class="kotak"><?= $number ?></div>
     <?php endforeach; ?>
+
+    <div class="clear"></div>
+
+    <!-- Latihan -->
+    <h4>List Students</h4>
+    <?php foreach ($students as $student) : ?>
+        <ul>
+            <li>Nama :<?php echo $student[0]; ?></li>
+            <li>NIM :<?php echo $student[1]; ?></li>
+            <li>Jurusan :<?php echo $student[2]; ?></li>
+            <li>Email :<?php echo $student[3]; ?></li>
+        </ul>
+    <?php endforeach; ?>
+
+
+    
+
+
 </body>
 
 </html>
