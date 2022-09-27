e4057400e352f790620e8c3a06a180e6b289cb63
raupsss <androghanz@gmail.com>
2022-09-27 08:45:54 +0700

    belajar

---
+++ belajar.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar</title>
+    <style>
+        div {
+            width: 50px;
+            height: 50px;
+            background-color: salmon;
+            text-align: center;
+            line-height: 50px;
+            margin: 3px;
+            float: left;
+            
+        }
+    </style>
+</head>
+<body>
+    <div>3</div>
+    <div>4</div>
+</body>
+</html>
